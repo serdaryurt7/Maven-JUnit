1ec1ee3fa3343f18d3bc9cb50ba84dc31a8b3f17
Serdar Yurtseven <serdaryurt7@gmail.com>
2024-08-02 09:21:39 +0300

    6.gun sonu

--- src/test/java/utilities/C05_DropdownMenu.java
+++ src/test/java/utilities/C05_DropdownMenu.java
@@ -1,14 +1,47 @@
 package utilities;
 
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
 public class C05_DropdownMenu extends TestBase {
 
-    //    https://testotomasyonu.com/form  adresine gidin
-    // 1. Dogum tarihi gün seçeneğinden index kullanarak 5'i seçin
-    // 2. Doğum tarihi ay seçeneğinden value olarak Nisan'ı seçin
-    // 3. Doğum tarihi yıl seçeneğinden visible text kullanarak 1990'i seçin
-    // 4. Seçilen değerleri konsolda yazdırın
-    // 5. Ay dropdown menüdeki tüm değerleri(value) yazdırın
-    // 6. Ay dropdown menusunun boyutunun 13 olduğunu test edin
+    @Test
+    public void dropdowntesti() throws InterruptedException {
 
-    // 2:24
+        //    https://testotomasyonu.com/form  adresine gidin
+        driver.get("https://testotomasyonu.com/form");
+
+        // 1. Dogum tarihi gün seçeneğinden index kullanarak 5'i seçin
+
+        WebElement gunDropdownMenu = driver.findElement(By.xpath("(//select[@class='form-control'])[1]"));
+        Select selectGun = new Select(gunDropdownMenu);
+        selectGun.selectByIndex(5);
+
+        // 2. Doğum tarihi ay seçeneğinden value olarak Nisan'ı seçin
+        WebElement ayDropdownMenu = driver.findElement(By.xpath("(//select[@class='form-control'])[2]"));
+        Select selectAy = new Select(ayDropdownMenu);
+        selectAy.selectByValue("nisan");
+
+        Thread.sleep(1000);
+
+        // 3. Doğum tarihi yıl seçeneğinden visible text kullanarak 1990'i seçin
+        WebElement yilDropdownMenu = driver.findElement(By.xpath("(//select[@class='form-control'])[3]"));
+        Select selectYil = new Select(yilDropdownMenu);
+        selectYil.selectByVisibleText("1990");
+
+        // 4. Seçilen değerleri konsolda yazdırın
+        System.out.println(selectGun.getFirstSelectedOption().getText());
+        System.out.println(selectAy.getFirstSelectedOption().getText());
+        System.out.println(selectYil.getFirstSelectedOption().getText());
+
+
+        // 5. Ay dropdown menüdeki tüm değerleri(value) yazdırın
+        // 6. Ay dropdown menusunun boyutunun 13 olduğunu test edin
+
+        Thread.sleep(5000);
+
+    }
+
 }
